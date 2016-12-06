f74061d2d78bbe95c964c1d7342ffb5214535d4c
yun.li <yun.li@Ctrip.com>
2016-12-06 19:45:12 +0800

    add UnitTestBase

---
+++ MoocSpring/test/java/com/imooc/test/base/UnitTestBase.java
@@ -0,0 +1,20 @@
+package java.com.imooc.test.base;
+
+import main.java.com.imooc.ioc.interfaces.OneInterface;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+/**
+ * Created by yun.li on 2016/12/6.
+ */
+@RunWith(BlockJUnit4ClassRunner.class)  //使用junit4进行测试
+
+public class UnitTestBase {
+
+    public UnitTestBase(String s) {
+    }
+
+    public OneInterface getBean(String s) {
+
+    }
+}
--- MoocSpring/test/java/com/imooc/test/ioc/interfaces/TestOneInterface.java
+++ MoocSpring/test/java/com/imooc/test/ioc/interfaces/TestOneInterface.java
@@ -1,7 +1,11 @@
 package test.java.com.imooc.test.ioc.interfaces;
 
+import main.java.com.imooc.ioc.interfaces.OneInterface;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+
+import java.com.imooc.test.base.UnitTestBase;
 
 /**
  * Created by yun.li on 2016/12/6.
@@ -9,5 +13,13 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class  TestOneInterface extends UnitTestBase{
+    public TestOneInterface(){
+        super("classpath*:spring-ioc.xml");
+    }
 
+    @Test
+    public void testHello(){
+        OneInterface oneInterface = super.getBean("oneInterface");
+        System.out.println(oneInterface.hello("我的输入参数"));
+    }
 }
